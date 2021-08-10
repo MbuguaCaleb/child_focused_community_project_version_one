db5f65f1388e9208398228bdf057e780c4c05020
Mbugua Caleb <mbuguacaleb30@gmail.com>
2021-08-10 20:55:21 +0300

    split-project-into-includes-and-sections

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,6 @@ class HomeController extends Controller
 
     public function index()
     {
-        return view('layouts.master');
+        return view('dashboard');
     }
 }
---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,10 @@
+<!-- start footer -->
+<div class="page-footer">
+    <div class="page-footer-inner"> 2018 &copy; Spice Hotel Template By
+        <a href="https://radixtouch.com/cdn-cgi/l/email-protection#493b2c2d3a3d283b3d212c242c092e24282025672a2624" target="_top" class="makerCss">RedStar Theme</a>
+    </div>
+    <div class="scroll-to-top">
+        <i class="icon-arrow-up"></i>
+    </div>
+</div>
+<!-- end footer -->
